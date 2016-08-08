7e1b59d140224b64767af6d7c370fa094e3e3b06
Leonard LiuTao <364125168@qq.com>
2016-08-08 16:09:54 +0800

    demo

---
+++ README.md
@@ -0,0 +1,8 @@
+react-router-show-a-component-demo
+==================================
+
+```
+1.bower install
+
+2.open index.html
+```
---
+++ app.js
@@ -0,0 +1,15 @@
+const App = React.createClass({
+    render: function () {
+        return (
+            <div>App</div>
+        );
+    }
+});
+
+
+ReactDOM.render(
+    <ReactRouter.Router>
+        <ReactRouter.Route path="/" component={App}>
+        </ReactRouter.Route>
+    </ReactRouter.Router>
+    , document.getElementById('content'));
